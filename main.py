1954d0228ce3e20dbda72662513718a7fbac6fe1
Titouan-Joseph REVOL <titouanjoseph@gmail.com>
2020-07-28 00:49:34 +0200

    some option, choose output file and custom domain

--- main.py
+++ main.py
@@ -1,20 +1,32 @@
 import argparse
+import os
 
-def mail(file):
+
+def mail(file, domain, outputFile):
+
+    if os.path.exists(outputFile):
+        print("Error file exists")
+        exit(-1)
+
+    if not outputFile.endswith(".txt"):
+        outputFile = outputFile + ".txt"
+
     with open(file, "r") as data:
         data.readline()
         for line in data.readlines():
             field = line.split(';')
             email = field[2].lower().strip().replace(" ", "") +\
                     '.' + field[1].lower().strip().replace(" ", "") +\
-                    "@insa-lyon.fr"
+                    "@" + domain
 
-            with open("liste.txt", "a") as output:
+            with open(outputFile, "a") as output:
                 output.write(email + ";")
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("file", help="csv file with ';' separator and gender;last name;fisrt name")
+    parser.add_argument("-d", "--domain", help="domain of email example: domain.com", default="insa-lyon.fr")
+    parser.add_argument("-o", "--output", help="output file", default="output.txt")
     args = parser.parse_args()
-    mail(args.file)
+    mail(args.file, args.domain, args.output)
